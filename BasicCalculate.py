072be9e18df4cc529e29deca3cb4200f8bee1f37
ApplicationEmpire <shreembrzee111@gmail.com>
2024-11-05 12:51:01 -0700

    Test1
    Test 1

---
+++ BasicCalculate.py
@@ -0,0 +1,117 @@
+def add (a,b):
+    return a+b
+
+print(add(1,2))
+
+def subtract (a,b):
+    return a-b
+
+print(subtract(1,2))
+
+def multiply (a,b):
+    return a*b
+
+print(multiply(1,2))
+
+def divide (a,b):
+    return a/b
+
+print(divide(1,2))
+
+def power (a,b):
+    return a**b
+
+print(power(1,2))
+
+def square (a):
+    return a**2
+
+print(square(2))
+
+def cube (a):
+    return a**3
+
+print(cube(2))
+
+def squareroot (a):
+    return a**(1/2)
+
+print(squareroot(2))
+
+def cuberoot (a):
+    return a**(1/3)
+
+print(cuberoot(2))
+
+def nthroot (a,b):
+    return a**(1/b)
+
+print(nthroot(2,3))
+
+def absolute (a):
+    return abs(a)
+
+print(absolute(-2))
+
+def negative (a):
+    return -a
+
+print(negative(2))
+
+def reciprocal (a):
+    return 1/a
+
+print(reciprocal(2))
+
+def factorial (a):
+    if a == 0:
+        return 1
+    else:
+        return a*factorial(a-1)
+
+print(factorial(5))
+
+def double (a):
+    return 2*a
+
+print(double(2))
+
+def halve (a):
+    return a/2
+
+print(halve(2))
+
+def third (a):
+    return a/3
+
+print(third(2))
+
+def quadruple (a):
+    return 2*a
+
+print(quadruple(2))
+
+def square (a):
+    return a**2
+
+print(square(2))
+
+def nthroot (a,b):
+    return a**(1/b)
+
+print(nthroot(2,3))
+
+def cuberoot (a):
+    return a**(1/3)
+
+print(cuberoot(2))
+
+def squareroot (a):
+    return a**(1/2)
+
+print(squareroot(2))
+
+def power (a,b):
+    return a**b
+
+print(power(1,2))
